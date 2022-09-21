7c23ef986bbfbddf0726f45d760483c6bf5cca19
martinmolinas <93428681+martinmolinas@users.noreply.github.com>
2022-09-21 07:48:27 +0200

    Add files via upload

---
+++ meinPseudoBATest Fehler.js
@@ -0,0 +1,30 @@
+// Defintion of Business Rule as function operation1
+var operation1 = function (manager,logger) {
+    var product = manager.getProductHome().getProductByID("LTASalesItem_105130");
+    var attribut = manager.getAttributeHome().getAttributeByID("LTAConsumerShortDescription");
+    if (attribute == null) {
+        logger.info("Attribute not found");
+    }
+    if (product == null) {
+        logger.info("Product not found");
+    } else {
+        var value = product.getValue(attribute.getID());
+        valu.setSimpleValue("100");
+    }
+}
+
+// Definition of test function
+var test = function(manager) {
+    // Definition of needed parameters for execution of operation1 function as objects
+    var logger = new Object()
+    // If the execution of the function / Business Action was not successful return error message, else return "Success"
+    try {
+         operation1(manager, logger)
+    } catch (e) {
+        return e.message;
+    }
+    return "Success" // muss also immer einen return-Wert geben!
+}
+
+// Execution of test function
+test(manager)
